09be063e9c7d11d7fd3d7c744bea7accff3a8ea5
CML120 <cheelor922@gmail.com>
2023-07-29 18:46:31 -0500

    worked on webpack config and service worker files, renamed service worker file as well

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generate HTML files based on index.html template
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        chunks: ['main'],
+        filename: 'JATE',
+      }),
+
+      // Inject the service worker into your app
+      new InjectManifest({
+        swSrc: './service-worker.js',
+        swDest: 'service-worker.js',
+      }),
+
+      // Generate the web app manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor app with PWA support',
+        start_url: './',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Add CSS loaders
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Babel loader for handling ES6
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
       ],
     },
   };
